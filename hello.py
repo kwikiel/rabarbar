f3975f5d1ddd41558706e2123b16886ea81afce6
Kacper Wikieł <k.wikiel@gmail.com>
2021-07-02 15:23:05 +0200

    feat: dynamic port

--- hello.py
+++ hello.py
@@ -1,9 +1,11 @@
 from flask import Flask, render_template
+import os
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return render_template('index.html')
 
+port = int(os.environ.get("PORT", 5000))
 if __name__ == '__main__':
-      app.run(host='0.0.0.0', port=1337)
+      app.run(host='0.0.0.0', port=port)
